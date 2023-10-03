30d1883dcfeaeaa6a0c7a2c3146707e06a9a6388
RamiroCovian <ramiro.covian@gmail.com>
2023-10-03 18:40:02 -0300

    TODO: para el jueves

--- balance/views.py
+++ balance/views.py
@@ -1,4 +1,5 @@
 import os
+from datetime import date
 from flask import render_template, request
 from . import app, RUTA
 from .forms import MovimientoForm
@@ -23,6 +24,17 @@ def eliminar(id):
 @app.route("/editar/<int:id>")
 def actualizar(id):
     if request.method == "GET":
-        formulario = MovimientoForm()
+        # TODO: Obtener el movimiento que se va a editar por si ID
+        # SELECT id, fecha, concepto, tipo, cantidad FROM movimientos WHERE id=?
+        # TODO: Acceder aqui por un enlace en la lista de movimientos
+        # (Al lado del boton de eliminar)
+        movimiento = {
+            "id": 55,
+            "fecha": date.fromisoformat("2023-10-03"),
+            "concepto": "Curso de formularios en Python",
+            "tipo": "G",
+            "cantidad": 55.95,
+        }
+        formulario = MovimientoForm(data=movimiento)
         return render_template("form_movimiento.html", form=formulario)
     return f"TODO: tratar el metodo POST para actualizar el movimiento {id}"
